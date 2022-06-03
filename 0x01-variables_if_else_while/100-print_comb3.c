444a152143f11274d797cf9571a07a32773489f3
mahletaw <mahletaw@gmail.com>
2022-06-03 03:33:54 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i < 9; i++)
+{
+for (j = i; j < 10; j++)
+{
+if (i == j)
+{
+continue;
+}
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+if (i == 8 && j == 9)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}}
+putchar('\n');
+
+return (0);
+}
