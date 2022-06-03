e867cef1952e78b20a8cc6758675774ae301718e
mahletaw <mahletaw@gmail.com>
2022-06-03 03:37:41 -0700

    print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Print the combination of three different single digit numbers
  *
- * Return: Always 0 (Success)
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-int i, j, k;
-for (i = 0; i <= 7; i++)
+int num, i, j;
+for (num = 0; num < 10; num++)
 {
-for (j = i + 1; j <= 8; j++)
+for (i = 0; i < 10; i++)
 {
-for (k = j + 1; k <= 9; k++)
+for (j = 0; j < 10; j++)
 {
-if (i == j && j == k)
+if (num == i || num == j || i == j || num > i || i > j)
 {
 continue;
 }
-putchar((i % 10) + '0');
-putchar((j % 10) + '0');
-putchar((k % 10) + '0');
-if (i == 7 && j == 8 && k == 9)
+else
+{
+putchar(num + '0');
+putchar(i + '0');
+putchar(j + '0');
+if (num == 7 && i == 8 && j == 9)
 {
 continue;
 }
 putchar(',');
 putchar(' ');
-}}}
+}
+}
+}
+}
 putchar('\n');
 return (0);
-}}
+}
