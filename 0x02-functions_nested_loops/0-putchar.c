0ff02d8c57467b4bd5ed85ff9581e527319e20ec
mahletaw <mahletaw@gmail.com>
2022-06-08 02:01:41 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,9 +8,10 @@ int main(void)
 {
 _putchar('_');
 _putchar('p');
-_putchar('u')  _putchar('t');
-  _putchar('c');
-  _putchar('h');
+_putchar('u');
+_putchar('t');
+ _putchar('c');
+ _putchar('h');
   _putchar('a');
   _putchar('r');
   _putchar('\n');
